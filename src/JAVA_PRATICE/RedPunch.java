4cdeffe82938cb8496092731249aa0e48516ef7c
Limdongkeun <lim961007@naver.com>
2022-05-03 15:33:55 +0900

    변경

--- src/JAVA_PRATICE/RedPunch.java
+++ src/JAVA_PRATICE/RedPunch.java
@@ -1,7 +1,15 @@
 package JAVA_PRATICE;
 
+import java.util.Random;
+
 public class RedPunch {
     public static void main(String[] args) {
-        System.out.println("bunny red punch");
+        Random random = new Random();
+
+        System.out.println(random.nextBoolean());
+        System.out.println(random.nextFloat());
+        System.out.println(random.nextInt());
+        System.out.println(random.nextInt(100));
+
     }
 }
