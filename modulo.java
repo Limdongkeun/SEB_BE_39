3dd676128f6d22040d213a6b759c3d5dce91d840
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-06-25 23:28:38 +0900

    Add files via upload

---
+++ modulo.java
@@ -0,0 +1,17 @@
+package SoloTest.codeStatesProblem;
+
+public class modulo {
+    public static void main(String[] args) {
+        Integer num1 = 123456789;
+        Integer num2 = 67;
+        Integer num3 = 0;
+
+        if (num3 == 0) {
+            System.out.println((String) null);
+        }
+        while (num1>=num2){
+            num1 = num1 - num2;
+        }
+        System.out.println(num1);
+    }
+}
